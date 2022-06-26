b17a262ffd44e6a6b52b038bd4c51c6aec4505c9
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-06-26 13:33:48 +0530

    file cleanup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import * as ReactDOMClient from "react-dom/client";
 
 // Call make Server
 makeServer();
 
-ReactDOM.render(
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
