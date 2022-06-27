b3297e4e0f4e19b55c8782c5f043b5bcd9a8f7ac
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-06-27 08:14:31 +0530

    style : navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./pages/index";
+import { Navbar } from "./components/index";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
       </Routes>
